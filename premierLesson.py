81783fec8cd755426b79731c28d33c2c2b4f2252
Erdem <erdemgocen44@gmail.com>
2024-05-21 12:51:28 +0200

    python ilk commit

--- premierLesson.py
+++ premierLesson.py
@@ -1,1 +1,24 @@
 print("hello world")
+
+isim="Erdem"
+
+mesaj="Hello"
+
+print("{} {} dedi...".format(isim,mesaj))
+
+sayi1=5
+sayi2=5.2
+sayi3=22/7
+
+print(type(sayi1))
+
+print(type(sayi2))
+
+
+print(sayi1**2)
+
+print(type(sayi3)) 
+
+print(16/5)
+
+print(16//5)
