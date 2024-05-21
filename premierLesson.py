5618cfec31df1fc0824bb90a58a981ea86ab0734
Erdem <erdemgocen44@gmail.com>
2024-05-21 13:07:00 +0200

    python ilk commit

--- premierLesson.py
+++ premierLesson.py
@@ -10,15 +10,26 @@ sayi1=5
 sayi2=5.2
 sayi3=22/7
 
-print(type(sayi1))
+print(type(sayi1))#yazdigimiz degerin tipini verir
 
 print(type(sayi2))
 
 
-print(sayi1**2)
+print(sayi1**2)#sayinin karesini alir eger 3 yazarsak kupunu alir
+
+print(3**4)#81 degerini verir
 
 print(type(sayi3)) 
 
-print(16/5)
+print(16/5)#reger iki // kullanirsak virgullu deger verir
 
-print(16//5)
+print(16//5)#tam sayi bolmesi yapar ve 3 yazar
+
+print(abs(-2))#sayinin mutlak degerini alir ABS float ve integer deger alir
+
+sayi4=22,7
+
+print(round(sayi4))#3 yazar yani yuvarla demek
+
+
+
