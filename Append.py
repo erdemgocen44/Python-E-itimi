f01e499f8bcb5f1056ff744fb9695cd953136e51
Erdem <erdemgocen44@gmail.com>
2024-05-22 10:31:49 +0200

    list

---
+++ Append.py
@@ -0,0 +1,18 @@
+#append metodu 
+#insert metodu
+#remove metodu
+#pop metodu
+#reverse metodu
+#sort ve sorted metotları
+
+renkler = ["Siyah","Beyaz","Yeşil","Mavi","Sari","Rouge","Rose","Violet","Vert"]
+
+print(renkler)
+
+renkler.append("Gri")#Listenin en SONUNA Gri ekler
+
+print(renkler)
+
+renkler.insert(0,"Yavruagzi")#0.indexe yavruagzini ekler
+
+print(renkler)
--- Listeler.py
+++ Listeler.py
@@ -2,7 +2,7 @@
 #Len fonksiyonu nedir?
 #Liste elemanlarına nasıl erişilebilir?
 #Liste nasıl parçalanır?
-renkler = ["Siyah","Beyaz","Yeşil","Mavi","Sari"]
+renkler = ["Siyah","Beyaz","Yeşil","Mavi","Sari","Rouge","Rose","Violet","Vert"]
 
 print(type(renkler))
 
@@ -14,6 +14,8 @@ print(renkler[:4])
 
 print(renkler[::2])#ikişer ikişer yap demek
 
-print(renkler[1:4:2])#birden dörde kadar ikişer say demek
+print(renkler[1::2])#birden sonuna kadar ikişer say demek
+
+print(renkler[1:6:2])#birden dörde kadar ikişer say demek
 
 
