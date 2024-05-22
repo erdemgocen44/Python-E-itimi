8e9f91db68963d67f73d374466e0b4e65304174b
Erdem <erdemgocen44@gmail.com>
2024-05-22 10:22:09 +0200

    list

---
+++ Listeler.py
@@ -0,0 +1,19 @@
+#Liste nedir ve nasıl oluşturulur? #Liste nasıl yazdırılır?
+#Len fonksiyonu nedir?
+#Liste elemanlarına nasıl erişilebilir?
+#Liste nasıl parçalanır?
+renkler = ["Siyah","Beyaz","Yeşil","Mavi","Sari"]
+
+print(type(renkler))
+
+print(renkler[0])
+
+print(renkler[0:])#sonuna kadar yazdır demek
+
+print(renkler[:4])
+
+print(renkler[::2])#ikişer ikişer yap demek
+
+print(renkler[1:4:2])#birden dörde kadar ikişer say demek
+
+
