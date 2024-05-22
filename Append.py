51745110e2181b60ea6b112e2fa98f0577b9dbf1
Erdem <erdemgocen44@gmail.com>
2024-05-22 10:38:36 +0200

    list

--- Append.py
+++ Append.py
@@ -1,9 +1,10 @@
-#append metodu 
-#insert metodu
-#remove metodu
-#pop metodu
-#reverse metodu
-#sort ve sorted metotları
+#append metodu Listenin en SONUNA eleman ekler
+#insert metodu indexi verdiğimiz yere ekler
+#remove metodu 
+#extend listeye birden fazla eleman ekler
+#pop metodu listenin en sonundaki elemanı siler ve bize soyler
+#reverse metodu listeyi tersinden sıralar
+#sort ve sorted metotları sırlama yapar
 
 renkler = ["Siyah","Beyaz","Yeşil","Mavi","Sari","Rouge","Rose","Violet","Vert"]
 
@@ -16,3 +17,24 @@ print(renkler)
 renkler.insert(0,"Yavruagzi")#0.indexe yavruagzini ekler
 
 print(renkler)
+
+renkler.remove("Sari")#Sari elemanı siler
+
+print(renkler)
+
+renkler2=["Turuncu","Pembe"]
+
+renkler.extend(renkler2)#renkler listesine renkler2 listesini ekler.
+
+print(renkler)
+
+silinen=renkler.pop()
+
+print(silinen)
+
+renkler.reverse()
+
+print(renkler)
+
+
+
