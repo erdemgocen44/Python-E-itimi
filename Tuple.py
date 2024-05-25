849f35979c8d383ab0a1bd4805471f2a25c99c05
Erdem <erdemgocen44@gmail.com>
2024-05-25 20:57:19 +0200

    set

--- Tuple.py
+++ Tuple.py
@@ -1,7 +1,11 @@
 # tuple demet demek 
 #liste gibi ama bunda normal parantez kullanılır
+#tuple demette herhangi bir eleman silinemez değiştirilemez eklenemez
+#hiçbir değişiklik uygulayamadıgımz LISTELERDIR
 
 demet=("Siyah","Beyaz","Yeşil","Mavi","Sari","Rouge","Rose")
-for renk in demet:
-print(renk)
+
+print(demet)
+
+print(type(demet))
 
